d22c64e6a266c25f2c49be92cd27e6ec062a0e99
Douglas A B Novato <douglasabnovato@gmail.com>
2021-12-14 11:33:48 -0300

    autenticação com firebase

--- letmeask/src/services/firebase.ts
+++ letmeask/src/services/firebase.ts
@@ -1,7 +1,6 @@
-import firebase from 'firebase/compat/app';
-
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from "firebase/database";
+import { getAuth} from "firebase/auth";
   
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -12,8 +11,10 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID
 };
- 
-firebase.initializeApp(firebaseConfig);
+   
+const app = initializeApp(firebaseConfig);
 
-export const auth = firebase.auth();
-export const database = firebase.database();
+const database = getDatabase(app);
+const auth = getAuth(app);
+
+export { database, auth }
