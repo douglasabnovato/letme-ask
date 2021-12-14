5f3a3e74547471933ef095f9c6683fefeab94587
Douglas A B Novato <douglasabnovato@gmail.com>
2021-12-14 10:27:52 -0300

    react-router-dom configurado e funcionando

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,14 +1,20 @@
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 import { Home } from "./pages/Home"
 import { NewRoom } from "./pages/NewRoom"
 
 function App() {
   return ( 
-    <Router>
-      <Route path="/" element={<Home />} /> 
-      <Route path="/room/new" element={<NewRoom />} /> 
-    </Router> 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/room/new" element={<NewRoom />} /> 
+      </Routes>
+  </BrowserRouter>
   );
 }
 
