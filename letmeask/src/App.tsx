49a6a70e2ac700f92665d79718ec4edf668e27fc
Douglas A B Novato <douglasabnovato@gmail.com>
2021-12-14 18:40:55 -0300

    Contexto de autenticação no App

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -8,25 +8,50 @@ import { Home } from "./pages/Home"
 import { NewRoom } from "./pages/NewRoom"
 import { auth, firebase} from "./services/firebase"
 
-export const AuthContext = createContext({} as any)
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
+type AuthContextType = {
+  user: User | undefined;
+  signInWithGoogle: () => void;
+}
+
+export const AuthContext = createContext({} as AuthContextType)
   
 function App() {
   
-  const [user, setUser] = useState()
+  const [user, setUser] = useState<User>()
 
   function signInWithGoogle(){
 
     const provider = new firebase.auth.GoogleAuthProvider()
 
     auth.signInWithPopup(provider).then((result) => {
-      console.log(result)
+      if(result.user){
+
+        const { displayName, photoURL, uid } = result.user
+
+        if(!displayName || !photoURL){
+          throw new Error("Missing information from Google Account.")
+        }
+
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL
+        })
+
+      }
     })
 
   }
 
   return ( 
       <BrowserRouter> 
-        <AuthContext.Provider value={{user, setUser}}>
+        <AuthContext.Provider value={{ user, signInWithGoogle }}>
           <Routes>
             <Route path="/" element={<Home />} /> 
             <Route path="/rooms/new" element={<NewRoom />} /> 
