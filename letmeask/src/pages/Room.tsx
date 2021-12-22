d899e3ca01a6ccb39264560dfeb695b581fc6909
Douglas A B Novato <douglasabnovato@gmail.com>
2021-12-22 19:06:52 -0300

    estrutura da sala, modo visitante

--- letmeask/src/pages/Room.tsx
+++ letmeask/src/pages/Room.tsx
@@ -1,5 +1,28 @@
+import logoImg from "../assets/logo.svg"
+import { Button } from "../components/Button"
+
 export function Room(){
     return (
-        <h1>Sala Criada</h1>
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <div>Código</div>
+                </div>
+            </header>
+            <main className="content">
+                <div className="room-title">
+                    <h1>Sala React</h1>
+                    <span>4 Perguntas</span>
+                </div>
+                <form>
+                    <textarea placeholder="O que você quer perguntar?" ></textarea>
+                    <div className="form-footer">
+                        <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+                        <Button type="submit">Enviar Pergunta</Button>
+                    </div>
+                </form>
+            </main>
+        </div>
     )
 }
